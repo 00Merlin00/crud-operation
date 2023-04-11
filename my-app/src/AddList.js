cb5de179e1d7856794033a964be312ffb922a729
Mohit Rautela <mrautela12@gmail.com>
2023-04-12 01:02:45 +0530

    some commit

--- my-app/src/AddList.js
+++ my-app/src/AddList.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Crud.css";
 
 export const AddList = ({ setLists }) => {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
   function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
     const price = event.target.elements.price.value;
+
     const newList = {
       id: 3,
       name,
@@ -15,11 +18,26 @@ export const AddList = ({ setLists }) => {
     setLists((prevList) => {
       return prevList.concat(newList);
     });
+    setInput1("");
+    setInput2("");
   }
+
   return (
     <form className="addForm" onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="Enter Name"></input>
-      <input type="text" name="price" placeholder="Enter Price"></input>
+      <input
+        type="text"
+        value={input1}
+        name="name"
+        placeholder="Enter Name"
+        onChange={(e) => setInput1(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        value={input2}
+        name="price"
+        placeholder="Enter Price"
+        onChange={(e) => setInput2(e.target.value)}
+      ></input>
       <button type="submit">Add</button>
     </form>
   );
---
+++ my-app/src/EditList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const EditList = () => {
+  return (
+    <tr>
+      <td>
+        <input type="text" name="name"></input>
+      </td>
+      <td>
+        <input type="text" name="price"></input>
+      </td>
+      <td>
+        <button></button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditList;
