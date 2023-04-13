fc1a43fafae800c6e9c95ebbc13d225e148c2910
Mohit Rautela <mrautela12@gmail.com>
2023-04-14 00:51:07 +0530

    some commit

---
+++ src/Crud.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./Crud.css";
+import { AddList } from "./AddList";
+import EditList from "./EditList";
+
+function Crud() {
+  const list = [
+    {
+      id: 1,
+      name: "HP",
+      price: "2222",
+    },
+    {
+      id: 2,
+      name: "Dell",
+      price: "2445",
+    },
+  ];
+
+  const [lists, setLists] = useState(list);
+  const [updateState, setUpdateState] = useState(-1);
+  function handleEdit(id) {
+    setUpdateState(id);
+  }
+
+  function handleDelete(id) {
+    const newList = lists.filter((li) => li.id !== id);
+    setLists(newList);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const price = event.target.elements.price.value;
+    const newList = lists.map((li) =>
+      li.id === updateState ? { ...li, name: name, price: price } : li
+    );
+
+    setLists(newList);
+    setUpdateState(-1);
+  }
+
+  return (
+    <div className="crud">
+      <div>
+        <AddList setLists={setLists} />
+        <form onSubmit={handleSubmit}>
+          <table>
+            <tbody>
+              {lists.map((current) =>
+                updateState === current.id ? (
+                  <EditList
+                    current={current}
+                    lists={lists}
+                    setLists={setLists}
+                  />
+                ) : (
+                  <tr>
+                    <td>{current.name}</td>
+                    <td>{current.price}</td>
+                    <td>
+                      <button
+                        onClick={() => handleEdit(current.id)}
+                        className="edit"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        type="submit"
+                        className="delete"
+                        onClick={() => handleDelete(current.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Crud;
---
+++ src/EditList.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const EditList = ({ current, lists, setLists }) => {
+  function handleInputName(event) {
+    const value = event.target.value;
+    const newList = lists.map((li) =>
+      li.id === current.id ? { ...li, name: value } : li
+    );
+
+    setLists(newList);
+  }
+  function handleInputPrice(event) {
+    const value = event.target.value;
+    const newList = lists.map((li) =>
+      li.id === current.id ? { ...li, price: value } : li
+    );
+
+    setLists(newList);
+  }
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          onChange={handleInputName}
+          name="name"
+          value={current.name}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          onChange={handleInputPrice}
+          name="price"
+          value={current.price}
+        ></input>
+      </td>
+      <td>
+        <button type="submit">Update</button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditList;
