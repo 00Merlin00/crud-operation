4c5e3767f0f02741d562c0cab94f6972ec9a8be4
Mohit Rautela <mrautela12@gmail.com>
2023-04-27 22:36:38 +0530

    some commit

--- my-app/src/Crud.js
+++ my-app/src/Crud.js
@@ -66,7 +66,7 @@ function Crud() {
                         Edit
                       </button>
                       <button
-                        type="submit"
+                        type="submi"
                         className="delete"
                         onClick={() => handleDelete(current.id)}
                       >
